f91d096f293ab51ef2e21160db8eeb4cf1782e42
dvd-azr <davidazhari73@gmail.com>
2024-09-18 16:46:09 +0700

    test env 3

--- index.js
+++ index.js
@@ -11,71 +11,71 @@ const port = 80;
 console.log("console : ", process.env.DB_NAME);
 
 // Database connection using Sequelize via the mysql2 driver
-const sequelize = new Sequelize(
-  dbConfig.DB,
-  dbConfig.USERNAME,
-  dbConfig.PASSWORD,
-  {
-    dialect: dbConfig.DIALECT,
-    host: dbConfig.HOST,
-    port: dbConfig.PORT,
-  }
-);
+// const sequelize = new Sequelize(
+//   dbConfig.DB,
+//   dbConfig.USERNAME,
+//   dbConfig.PASSWORD,
+//   {
+//     dialect: dbConfig.DIALECT,
+//     host: dbConfig.HOST,
+//     port: dbConfig.PORT,
+//   }
+// );
 
-sequelize.sync().then(
-  function () {
-    console.log("DB connection sucessful.");
-  },
-  function (err) {
-    // catch error here
-    console.log(err);
-  }
-);
+// sequelize.sync().then(
+//   function () {
+//     console.log("DB connection sucessful.");
+//   },
+//   function (err) {
+//     // catch error here
+//     console.log(err);
+//   }
+// );
 
-// Let's define a basic User model
-const User = sequelize.define("User", {
-  email: Sequelize.STRING,
-  firstName: Sequelize.STRING,
-  lastName: Sequelize.STRING,
-});
+// // Let's define a basic User model
+// const User = sequelize.define("User", {
+//   email: Sequelize.STRING,
+//   firstName: Sequelize.STRING,
+//   lastName: Sequelize.STRING,
+// });
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully.");
-//   })
-//   .catch((error) => {
-//     console.error("Unable to connect to the database: ", error);
-//   });
+// // sequelize
+// //   .authenticate()
+// //   .then(() => {
+// //     console.log("Connection has been established successfully.");
+// //   })
+// //   .catch((error) => {
+// //     console.error("Unable to connect to the database: ", error);
+// //   });
 
-// Match the DB state with the actual model definition on the top: https://sequelize.org/docs/v6/core-concepts/model-basics/#model-synchronization
-User.sync({ alter: true });
+// // Match the DB state with the actual model definition on the top: https://sequelize.org/docs/v6/core-concepts/model-basics/#model-synchronization
+// User.sync({ alter: true });
 
-// Express default body parser
-app.use(express.json());
+// // Express default body parser
+// app.use(express.json());
 
-// Create a new User
-app.post("/users", (req, res) => {
-  const { email, firstName, lastName } = req.body;
-  User.create({ email, firstName, lastName })
-    .then((savedUser) => {
-      res.json(savedUser);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
-});
+// // Create a new User
+// app.post("/users", (req, res) => {
+//   const { email, firstName, lastName } = req.body;
+//   User.create({ email, firstName, lastName })
+//     .then((savedUser) => {
+//       res.json(savedUser);
+//     })
+//     .catch((err) => {
+//       res.status(500).json(err);
+//     });
+// });
 
-// Get all users
-app.get("/users", (req, res) => {
-  User.findAll()
-    .then((users) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
-});
+// // Get all users
+// app.get("/users", (req, res) => {
+//   User.findAll()
+//     .then((users) => {
+//       res.json(users);
+//     })
+//     .catch((err) => {
+//       res.status(500).json(err);
+//     });
+// });
 
 app.get("/status", (req, res) => {
   res.json({
